b84d3632de3e9217d4f5bc5e97a2711fa4e21f5d
4rryaman3 <shreearryaman@gmail.com>
2025-04-20 14:08:37 +0530

    customized the dashboard

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-	ensure_installed = {"lua_ls","pyright","harper_ls","html","spectral","dockerls"}
+	ensure_installed = {"lua_ls","pyright","harper_ls","html","dockerls"}
 }
 
 
